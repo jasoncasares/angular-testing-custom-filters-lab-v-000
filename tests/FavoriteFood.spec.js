8cace2d290e8d9be558c9d37d2ca3a11aff097d4
jasoncasares <jasonc@csdrafting.com>
2016-10-22 14:36:52 -0700

    Done.

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -1,11 +1,29 @@
 describe('Favorite Food Filter', function () {
-	var $controller;
+	var $filter;
 
 	beforeEach(module('app'));
 
-	beforeEach(inject(function ($injector) {
-		$filter = $injector.get('$filter');
-	}));
+	beforeEach(function () {
+    inject(function (_$filter_) {
+      $filter = _$filter_;
+    });
+	});
 
-	
+	it('should filter the food', function () {
+	  var people = [
+      {
+        name: 'Brian',
+        favoriteFood: 'Pizza'
+      },
+      {
+        name: 'Loriann',
+        favoriteFood: 'Sugar'
+      }
+    ];
+
+    result = $filter('favoriteFood')(people, 'Pizza');
+
+    expect(result[0].favoriteFood).toEqual('Pizza');
+    expect(result[0].name).toEqual('Brian');
+	});
 });
--- tests/removeAllVowels.spec.js
+++ tests/removeAllVowels.spec.js
@@ -1,10 +1,19 @@
 describe('removeAllVowels Filter', function () {
-	var $controller;
+	var $filter;
 
 	beforeEach(module('app'));
 
-	beforeEach(inject(function ($injector) {
-		$filter = $injector.get('$filter');
-	}));
+	beforeEach(function () {
+    inject(function (_$filter_) {
+      $filter = _$filter_;
+    });
+	});
 
+	it('should filter the food', function () {
+	  var string = "Here is a string to remove some vowels.";
+
+    result = $filter('removeAllVowels')(string);
+
+    expect(result).toEqual('Hr s  strng t rmv sm vwls.');
+	});
 });
